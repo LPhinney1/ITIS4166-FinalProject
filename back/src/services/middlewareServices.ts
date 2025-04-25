e870358f742faa39156b90cc87ae592a0568c8ca
LPhinney1 <lphinne1@uncc.edu>
2025-04-25 11:34:09 -0400

    removed error message txt (for security?)

--- back/src/services/middlewareServices.ts
+++ back/src/services/middlewareServices.ts
@@ -5,7 +5,7 @@ export async function verifyAuthToken(token: string): Promise<number> {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: number };
         const user = await getUserById(decoded.userId);
-        if (!user) throw new Error('User not found');
+        if (!user) throw new Error();
         return decoded.userId;
     } catch (err) {
         throw new Error('Invalid token');
