ba1e3943cfc5bd636cdc60fabfed5af4d7364b9d
Avdey7 <aakseno1@charlotte.edu>
2025-05-06 19:43:01 -0400

    added so that we don't have to reload (the changes apply automatically)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { UserProvider } from './context/UseUser.tsx';
+import { DataRefreshProvider } from './context/DataRefreshContext';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <UserProvider>
-            <App />
+            <DataRefreshProvider>
+                <App />
+            </DataRefreshProvider>
         </UserProvider>
     </StrictMode>,
 );
