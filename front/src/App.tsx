79fb0557d09bcbfdb6225fdcbf4e19bacbcb1c95
loganphinney <logan.phinney10@gmail.com>
2025-05-07 11:01:17 -0400

    fixed line centering

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         return (
             <div style={{ padding: '2em', maxWidth: '500px', margin: '0 auto' }}>
                 <AuthForm onLogin={login} />
-                <em style={{ fontSize: 'small' }}>Backend may take ~20 seconds to spin up if inactive for long period.</em>
+                <em style={{ fontSize: 'small', textAlign: 'center', display: 'block' }}>Backend may take ~20 seconds to spin up if inactive for long period.</em>
             </div>
         );
     }
